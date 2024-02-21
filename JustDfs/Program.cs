0827d38bbbbe562dd7f31e0bdbe6d679f6b62198
DruzhinD <d.druzhin04@gmail.com>
2024-02-21 15:03:45 +1000

    проблема с зацикливанием

--- JustDfs/Program.cs
+++ JustDfs/Program.cs
@@ -19,7 +19,7 @@ namespace JustDfs
                 { 0, 0, 0, 0, 1, 0, 1 }, //5
                 { 0, 0, 0, 0, 1, 1, 0 }, //6
             };
-            string res = DfsString(matrix, 6);
+            string res = DfsString(matrix, 4);
             Console.WriteLine(res);
         }
 
@@ -32,8 +32,8 @@ namespace JustDfs
             string result = "";
             int curInd = vertInd; //текущий индекс
 
-            stack.Push(curInd);
-            result += curInd;
+            //stack.Push(curInd);
+            //result += curInd;
             do
             {
                 //флаг, указывающий на наличие соседей, где false - их нет, true - сосед найден
@@ -43,15 +43,16 @@ namespace JustDfs
                 for (int i = 0; i < matrix.GetLength(1); i++)
                 {
                     //если в матрице смежности указана единица, т.е. есть ребро И
-                    
+                    //ЗАЦИКЛИВАЕТСЯ на 4211111111111111111111
                     if (matrix[curInd, i] == 1 && !stack.Contains(i) && !result.Contains(i.ToString()))
                     {
+                        result += curInd;
+                        stack.Push(curInd);
+                        curInd = i;
                         neighbourFlag = true;
-                        stack.Push(i);
-                        curInd = i; //была проблема с этим, переменная оставалась == 0
 
                         //если этой вершины нет в конечном списке вершин, то добавляем её туда
-                        result += i;
+                        //result += i;
                         break;
                     }
                 }
@@ -59,9 +60,9 @@ namespace JustDfs
                 //если соседи не найдены, то достаем текущую вершину из стека
                 if (!neighbourFlag)
                 {
-                    stack.Pop();
+                    curInd = stack.Pop();
                     
-                    if (stack.Count > 0)
+                    if (stack.Count > 100)
                     {
                         curInd = stack.Pop();
                         stack.Push(curInd);
