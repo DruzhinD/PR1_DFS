92e528bc020a370d3a7769abfa3dc5444620b564
HomePC <dimon12.dry@gmail.com>
2024-02-25 22:59:52 +1000

    3 задание: начало положено

---
+++ Stirlitz/StirlitzProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stirlitz
+{
+    internal class StirlitzProgram
+    {
+        static void Main(string[] args)
+        {
+            //кол-во площадей
+            int amount = 5;
+            //матрица расстояний (смежности)
+            int[] distances = new int[]
+            {
+                0, 1, 9, 9, 2,
+                1, 0, 9, 9, 9,
+                9, 9, 0, 9, 9,
+                9, 9, 9, 0, 9,
+                2, 9, 9, 9, 0,
+            };
+
+        }
+    }
+}
